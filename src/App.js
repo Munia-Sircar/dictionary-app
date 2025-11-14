9e71eddc1c67ea91a33759ead53eef9fe82a4f85
Munia Sircar <moonish.nutcase1991@gmail.com>
2025-11-13 19:09:03 -0600

    added netlify link

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">Hello Everyone!</header>
+      <a
+        href="https://moon-dictionary-app.netlify.app/"
+        className="netlify-link"
+        target="_blank">
+        Netlify
+      </a>
     </div>
   );
 }
