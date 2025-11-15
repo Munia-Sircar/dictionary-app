ce12c0da1c3f7964bbe804247aadd18b77801d96
Munia Sircar <moonish.nutcase1991@gmail.com>
2025-11-14 21:08:45 -0600

    added footer

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello Everyone!</header>
-      <p>Welcome to my Dictionary App.</p>
-      <a
-        href="https://moon-dictionary-app.netlify.app/"
-        className="netlify-link"
-        target="_blank"
-        rel="noreferrer">
-        Netlify
-      </a>
+      <div className="container">
+        <header className="App-header">Hello Everyone!</header>
+        <p>Welcome to my Dictionary App.</p>
+        <footer>
+          This project is coded by Munia Sircar🌹, hosted on
+          <a
+            href="https://moon-dictionary-app.netlify.app/"
+            className="netlify-link"
+            target="_blank"
+            rel="noreferrer">
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
