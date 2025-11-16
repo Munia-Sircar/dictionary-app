1055cc96e0d142580bb43b6f0be9d2194c2999f8
Munia Sircar <moonish.nutcase1991@gmail.com>
2025-11-16 11:00:37 -0600

    moved API integration inside a function

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,22 +4,24 @@ import axios from "axios";
 
 export default function Dictionary() {
   let [word, setWord] = useState(null);
+
+  function apiCall(response) {
+    console.log(response);
+  }
+
   function submitWord(event) {
     event.preventDefault();
     alert(word);
+
+    let key = "28d0bff0ffaa52b09e33da6etode543b";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${word}&key=${key}`;
+
+    axios.get(apiUrl).then(apiCall);
   }
 
   function handleWordChange(event) {
     setWord(event.target.value);
   }
-
-  function apiCall(response) {
-    console.log(response);
-  }
-  let key = "28d0bff0ffaa52b09e33da6etode543b";
-  let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=love&key=${key}`;
-
-  axios.get(apiUrl).then(apiCall);
 
   return (
     <div className="dictionary">
