ddf28a6f991f6d680903ecec8dab38da442494c2
Munia Sircar <moonish.nutcase1991@gmail.com>
2025-11-16 15:26:53 -0600

    resized fonts

--- src/Results.js
+++ src/Results.js
@@ -5,7 +5,9 @@ export default function Results(props) {
   return (
     <div className="Results">
       <h3>{props.results.word}</h3>
-      <em>{props.results.phonetic}</em>
+      <h5>
+        <em>{props.results.phonetic}</em>
+      </h5>
       <h6>{props.results.meanings[0].definition}</h6>
     </div>
   );
